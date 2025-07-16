045f5d050128ab859562ce1f9a953eb6729b2eb0
Mbk10298 <64392360+Mbk10298@users.noreply.github.com>
2025-07-16 18:19:33 -0400

    Sickness bar script b42

--- Contents/mods/Minimal Display Bars Nutritions B42/common/media/lua/client/Minimal Display Bars (A).lua
+++ Contents/mods/Minimal Display Bars Nutritions B42/common/media/lua/client/Minimal Display Bars (A).lua
@@ -652,6 +652,32 @@ local DEFAULT_SETTINGS = {
         ["showImage"] = false,
         ["isIconRight"] = false,
     },
+	["sickness"] = {
+        ["x"] = 70 + 15,
+        ["y"] = 30,
+        ["width"] = 8,
+        ["height"] = 150,
+        ["l"] = 2,
+        ["t"] = 3,
+        ["r"] = 2,
+        ["b"] = 3,
+        ["color"] = {red = (150 / 255), 
+                    green = (255 / 255), 
+                    blue = (10 / 255), 
+                    alpha = 0.75},
+        ["isMovable"] = true,
+        ["isResizable"] = false,
+        ["isVisible"] = true,
+        ["isVertical"] = true,
+        ["alwaysBringToTop"] = false,
+        ["showMoodletThresholdLines"] = true,
+        ["isCompact"] = false,
+        ["imageShowBack"] = true,
+        ["imageName"] = "media/ui/Moodles/Moodle_Icon_Sick.png",
+        ["imageSize"] = 22,
+        ["showImage"] = false,
+        ["isIconRight"] = false,
+    },	
     ["thirst"] = {
         ["x"] = 85 + (8 * 1),
         ["y"] = 30,
@@ -1150,6 +1176,28 @@ local function getColorHunger(isoPlayer)
     return color
 end
 
+-- Sickness Functions
+local function calcSickness(value)
+    return value
+end
+local function getSickness(isoPlayer, useRealValue) 
+    if useRealValue then
+        return isoPlayer:getStats():getSickness()
+    else
+        if isoPlayer:isDead() then
+            return -1
+        else
+            return calcSickness( isoPlayer:getStats():getSickness() )
+        end
+    end
+end
+
+local function getColorSickness(isoPlayer) 
+    local color
+    color = MinimalDisplayBars.configTables[isoPlayer:getPlayerNum() + 1]["sickness"]["color"]
+    return color
+end
+
 -- Thirst Functions
 local function calcThirst(value)
     return 1 - value
@@ -1552,6 +1600,12 @@ local function getMoodletThresholdTables()
             [3] = calcHunger(0.45),
             [4] = calcHunger(0.70),
         },
+		["sickness"] = {
+            [1] = calcSickness(.25), -- 25 / 100
+            [2] = calcSickness(.50),
+            [3] = calcSickness(.75),
+            [4] = calcSickness(.90),
+        },		
         ["thirst"] = {
             [1] = calcThirst(0.12), -- 0.12 / 1.00
             [2] = calcThirst(0.25),
@@ -3212,6 +3266,20 @@ local function createUiFor(playerIndex, isoPlayer)
         nil)
     bar15:initialise()
     bar15:addToUIManager()
+	
+	local idName16 = "sickness"
+    local bar16 = ISGenericMiniDisplayBar:new(
+        idName16, 
+        MinimalDisplayBars.configFileLocations[coopNum], 
+        playerIndex, isoPlayer, coopNum, 
+        MinimalDisplayBars.configTables[coopNum], 
+        xOffset, yOffset, 
+        nil, 
+        getSickness,
+        getColorSickness, true,
+        moodletThresholdTables[idName16])
+    bar16:initialise()
+    bar16:addToUIManager()  
     
     
     -- Add all valid display bars to a Global varible to be shared.
@@ -3231,6 +3299,7 @@ local function createUiFor(playerIndex, isoPlayer)
         [idName13] = bar13,
         [idName14] = bar14,
         [idName15] = bar15,
+		[idName16] = bar16,
     }
     
     ------------------------------------------------------------------------------
